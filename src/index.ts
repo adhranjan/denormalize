f083605bc3a0aa65bbd12e7bd3826e0acbd0386c
Ranjan Adhikari <ranjan@evvoiot.com>
2020-10-17 01:19:19 +0530

    Initial COmmit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// import express from 'express';
+
+// const server = express();
+
+
+// server.use('/_healthcheck', (_req, res) => {
+//   res.status(200).json({ uptime: process.uptime() });
+// });
+
+// server.listen(4004, () => { console.log('Running at localhost:4004') })
+
+console.log('this is fresh')
+console.log('121');
